db61ee4a2b370631a34ebe0679db8ab3292181e5
Ebubeker Rexha <60944813+Ebubeker@users.noreply.github.com>
2025-05-23 10:19:12 +0200

    initialization

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Create a python environment
+
+`python -m venv ven`
+
+# Activate the python environment
+
+`venv/Scripts/Activate`
+
+# Install all the required libraries
+
+`pip install -r requirements.txt`
---
+++ visualization.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+print("Visualization")
+
+# Dataframe
+df = pd.read_csv("dataset_HR.csv")
+
+print(df.head())
+
+#TODO: Flavio implement the visualization functions here
